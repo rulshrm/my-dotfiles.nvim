167ff62a9c56ccb090356bd9acc58f3fc403cac5
rulshrm <raflysahrul3114@gmail.com>
2025-05-04 17:25:44 +0700

    feat(null-ls): enhance setup with improved logging and refactor source registration

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -3,26 +3,22 @@ local M = {}
 -- Daftar alat yang akan diinstal oleh Mason
 M.ensure_installed = {
   -- LSPs
-  "lua_ls",                   -- Lua LSP
-  "html",                     -- HTML LSP
-  "cssls",                    -- CSS LSP
-  "ts_ls",                 -- TypeScript/JavaScript LSP
-  "jsonls",                   -- JSON LSP
-  "yamlls",                   -- YAML LSP
-  "vimls",                    -- Vimscript LSP
-  "rust_analyzer",            -- Rust LSP
-  "gopls",                    -- Go LSP
-  "nimls",                    -- Nim LSP
+  "lua-language-server",          -- Lua LSP
+  "html-lsp",                     -- HTML LSP
+  "css-lsp",                      -- CSS LSP
+  "typescript-language-server",    -- TypeScript/JavaScript LSP
+  "json-lsp",                     -- JSON LSP
+  "yaml-language-server",         -- YAML LSP
+  "vim-language-server",          -- Vimscript LSP
+  "rust-analyzer",                -- Rust LSP
+  "gopls",                        -- Go LSP
+  "nimlsp",                       -- Nim LSP
 
-  "rustfmt",                  -- Rust formatter
-  "shfmt",                    -- Shell script formatter
-  "black",                    -- Python formatter
-
-  -- Linters
-  "eslint_d",                 -- ESLint daemon for JS/TS
-  "ruff",                     -- Python linter/formatter
-  "shellcheck",               -- Shell script linter
-  "hadolint",                 -- Dockerfile linter
+  -- Formatters & Linters
+  "prettier",                     -- Prettier formatter
+  "stylua",                      -- Lua formatter
+  "eslint_d",                    -- JavaScript/TypeScript linter
+  "shellcheck",                  -- Shell script linter
 }
 
 M.setup = function()
@@ -41,24 +37,15 @@ M.setup = function()
         package_pending = "➜",
         package_uninstalled = "✗",
       },
-      keymaps = {
-        toggle_server_expand = "<CR>",
-        install_server = "i",
-        update_server = "u",
-        uninstall_server = "x",
-      },
     },
     max_concurrent_installers = 10,
-    pip = {
-      install_args = {},
-    },
   })
 
   -- Setup Mason LSPConfig
   mason_lspconfig.setup({
     ensure_installed = {
       "lua_ls",
-      "ts_ls",
+      "tsserver",  -- Changed from ts_ls to tsserver
       "html",
       "cssls",
       "jsonls",
@@ -79,47 +66,6 @@ M.setup = function()
 
           -- Notify when LSP attaches
           vim.notify(string.format("LSP %s attached", server_name), vim.log.levels.INFO)
-        end,
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-      })
-    end,
-
-    -- Special configuration for specific servers
-    ["lua_ls"] = function()
-      require("lspconfig").lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-            workspace = {
-              library = {
-                vim.fn.expand("$VIMRUNTIME/lua"),
-                vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
-              },
-            },
-          },
-        },
-      })
-    end,
-
-    ["ts_ls"] = function()
-      require("lspconfig").ts_ls.setup({
-        on_attach = function(client, bufnr)
-          -- Disable tsserver formatting
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-
-          -- Basic LSP keybindings
-          local opts = { noremap = true, silent = true, buffer = bufnr }
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-
-          -- Notify when tsserver attaches
-          vim.notify("TypeScript LSP attached", vim.log.levels.INFO)
         end,
         capabilities = require("cmp_nvim_lsp").default_capabilities(),
       })
--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -6,7 +6,7 @@ local M = {}
 
 M.setup = function()
   -- Debug log
-  print("Setting up null-ls...")
+  notify("Setting up null-ls...", vim.log.levels.INFO)
 
   -- Verifikasi prettierd terinstall
   if vim.fn.executable("prettierd") ~= 1 then
@@ -14,58 +14,46 @@ M.setup = function()
     return
   end
 
-  -- Register sources
-  local sources = {
-    -- Formatting
-    null_ls.builtins.formatting.prettierd.with({
-      filetypes = {
-        "javascript",
-        "typescript",
-        "javascriptreact",
-        "typescriptreact",
-        "json",
-        "css",
-        "html",
-        "yaml",
-        "markdown"
-      },
-    }),
-    
-    -- Linting
-    null_ls.builtins.diagnostics.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
-      end,
-    }),
-  }
-
+  -- Setup null-ls
   null_ls.setup({
-    debug = true, -- Enable debug mode
-    sources = sources,
+    debug = true,
+    sources = {
+      -- Formatting
+      null_ls.builtins.formatting.prettierd.with({
+        filetypes = {
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "json",
+          "css",
+          "html",
+          "yaml",
+          "markdown"
+        },
+      }),
+      
+      -- Linting
+      null_ls.builtins.diagnostics.eslint_d.with({
+        condition = function(utils)
+          return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
+        end,
+      }),
+    },
+    
+    -- Perbaiki on_attach handler
     on_attach = function(client, bufnr)
       -- Debug log
-      print(string.format("null-ls attaching to buffer %d", bufnr))
+      notify(string.format("null-ls attaching to buffer %d", bufnr), vim.log.levels.INFO)
       
+      -- Format on save
       if client.supports_method("textDocument/formatting") then
-        -- Notify when null-ls attaches to buffer
-        notify(string.format("null-ls attached to buffer %d", bufnr), vim.log.levels.INFO)
-        
-        -- Set formatting keymap
-        vim.keymap.set("n", "<leader>f", function()
-          vim.lsp.buf.format({
-            async = true,
-            filter = function(c)
-              return c.name == "null-ls"
-            end,
-          })
-        end, { buffer = bufnr, desc = "Format with null-ls" })
-
-        -- Set up format on save
         vim.api.nvim_create_autocmd("BufWritePre", {
           buffer = bufnr,
           callback = function()
             vim.lsp.buf.format({
               async = false,
+              timeout_ms = 2000,
               filter = function(c)
                 return c.name == "null-ls"
               end,
@@ -76,8 +64,7 @@ M.setup = function()
     end,
   })
 
-  -- Debug log
-  print("null-ls setup completed!")
+  notify("null-ls setup completed!", vim.log.levels.INFO)
 end
 
 return M
