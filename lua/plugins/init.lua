d129bb7b94bcb7f63fb0bf412d69816b37738243
rulshrm <raflysahrul3114@gmail.com>
2025-05-14 14:37:05 +0700

    fix(init.lua): reorganize core plugin definitions and improve comments for clarity

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,10 +7,10 @@
 --]]
 
 return {
-  -- Core plugins dengan lazy = false untuk load saat startup
+  -- Core dependencies
   {
     "nvim-lua/plenary.nvim",
-    lazy = false, -- Required by many plugins, load immediately
+    lazy = false,
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -18,54 +18,14 @@ return {
     build = ":TSUpdate",
   },
 
-  -- Plugins yang bisa di-lazy load
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-    }
-  },
-
-  -- PHP/Laravel Development
-  {
-    "adalessa/laravel.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "tpope/vim-dotenv",
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = { "Artisan", "Composer" },
-    keys = {
-      { "<leader>la", ":Artisan<cr>" },
-      { "<leader>lr", ":Artisan route:list<cr>" },
-      { "<leader>lm", ":Artisan make:" },
-      { "<leader>lt", ":Artisan tinker<cr>" },
-    },
-    config = function()
-      require("laravel").setup()
-    end,
-  },
-  {
-    "jwalton512/vim-blade",
-    ft = "blade",
-  },
-  {
-    "tpope/vim-dispatch",
-    cmd = { "Dispatch", "Make" },
-  },
-  {
-    "noahfrederick/vim-composer",
-    dependencies = { "tpope/vim-dispatch" },
-    ft = "php",
-  },
-
-  -- Group plugins berdasarkan event/command
-  { import = "plugins.lsp", event = { "BufReadPre", "BufNewFile" } },
-  { import = "plugins.completion", event = "InsertEnter" },
-  { import = "plugins.ui", event = "VeryLazy" },
-  { import = "plugins.coding", event = { "BufReadPre", "BufNewFile" } },
-  { import = "plugins.utils", event = "VeryLazy" },
+  -- Plugin category imports
+  { import = "plugins.lsp" },         -- LSP support
+  { import = "plugins.completion" },   -- Autocompletion
+  { import = "plugins.php" },         -- PHP Development
+  { import = "plugins.coding" },      -- Code analysis tools
+  { import = "plugins.editor" },      -- Editor features
+  { import = "plugins.ui" },          -- UI components
+  { import = "plugins.utils" },       -- Utility functions
+  { import = "plugins.testing" },     -- Testing frameworks
+  { import = "plugins.project" },     -- Project management
 }
