41a050f4fc5c5257794afee9ef45038dcb8c35cb
rulshrm <raflysahrul3114@gmail.com>
2025-06-25 08:00:01 +0700

    feat(lua): update nvim-ts-autotag options to include additional filetypes

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -27,9 +27,13 @@ return {
     opts = {
       autotag = {
         enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
         filetypes = {
           'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 
-          'svelte', 'vue', 'tsx', 'jsx', 'xml',
+          'svelte', 'vue', 'tsx', 'jsx', 'xml', 'php', 'markdown', 
+          'astro', 'glimmer', 'handlebars', 'hbs'
         },
       }
     }
