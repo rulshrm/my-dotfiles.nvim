828fad65fb9ffb96870a21a845412c636990f80b
rulshrm <raflysahrul3114@gmail.com>
2025-08-01 22:51:07 +0700

    feat(chunk): refactor hlchunk setup for improved structure and readability

--- lua/configs/chunk.lua
+++ lua/configs/chunk.lua
@@ -1,13 +1,54 @@
-require("hlchunk").setup({
-  chunk = {
-        enable = true,
-    chars = {
+local M = {}
+
+M.setup = function()
+  require("hlchunk").setup({
+    chunk = {
+      enable = true,
+      support_filetypes = {
+        "*.js",
+        "*.jsx",
+        "*.ts",
+        "*.tsx",
+        "*.vue",
+        "*.svelte",
+        "*.lua",
+        "*.*"
+      },
+      chars = {
         horizontal_line = "─",
-        -- vertical_line = "│",
-        left_top = "┌",
-        left_bottom = "└",
+        vertical_line = "│",
+        left_top = "╭",
+        left_bottom = "╰",
         right_arrow = "─",
+      },
+      style = {
+        { fg = "#806d9c" },
+      },
     },
-  }
-})
+    indent = {
+      enable = true,
+      use_treesitter = true,
+      chars = {
+        "│",
+      },
+      style = {
+        { fg = "#2d3343" },
+      },
+    },
+    line_num = {
+      enable = true,
+    },
+    blank = {
+      enable = true,
+      chars = {
+        "·",
+      },
+      style = {
+        { fg = "#2d3343" },
+      },
+    },
+  })
+end
+
+return M
 
