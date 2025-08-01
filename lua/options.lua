9bb0a2931949f1da9759948af63ec85487dd6a09
rulshrm <raflysahrul3114@gmail.com>
2025-08-01 23:23:08 +0700

    feat(options): update highlight settings for hlchunk and clean up comments

--- lua/options.lua
+++ lua/options.lua
@@ -3,18 +3,19 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both'
 
 local lsp_vim = vim.lsp
 lsp_vim.set_log_level("off")
 
-vim.api.nvim_set_hl(0, "IblChar", { fg = "#5c6370" }) -- Sesuaikan warna sesuai preferensi Anda
-vim.api.nvim_set_hl(0, "IblScopeChar", { fg = "#61afef" }) -- Sesuaikan warna sesuai preferensi Anda
+-- highlight untuk hlchunk
+vim.api.nvim_set_hl(0, "HlchunkIndent", { fg = "#2d3343" })
+vim.api.nvim_set_hl(0, "HlchunkScope", { fg = "#61afef" })
 
 -- Highlight untuk import-cost.nvim
-vim.api.nvim_set_hl(0, "ImportCostSmall", { fg = "#98c379", bold = true }) -- Green
-vim.api.nvim_set_hl(0, "ImportCostMedium", { fg = "#e5c07b", bold = true }) -- Yellow
-vim.api.nvim_set_hl(0, "ImportCostLarge", { fg = "#e06c75", bold = true }) -- Red
+vim.api.nvim_set_hl(0, "ImportCostSmall", { fg = "#98c379", bold = true })
+vim.api.nvim_set_hl(0, "ImportCostMedium", { fg = "#e5c07b", bold = true })
+vim.api.nvim_set_hl(0, "ImportCostLarge", { fg = "#e06c75", bold = true })
 
 -- Text wrap configuration
 vim.opt.wrap = true               -- Enable line wrapping
